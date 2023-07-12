70c319bb02969f645372f8df70add2da6d5f6932
Olamide <olamide@Zipcoders-MacBook-Pro-26.local>
2023-07-12 18:48:22 -0400

    done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,80 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new LinkedHashMap<>());
     }
+    //nullary constructor ser this object to a Treemap when no parameters are provided
 
     public void add(String name, String phoneNumber) {
+        if (phonebook.containsKey(name)) {
+            phonebook.get(name).add(phoneNumber);
+
+        } else {
+            phonebook.put(name, new ArrayList<>());
+            phonebook.get(name).add(phoneNumber);
+
+        }
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
-    }
+        for (String s : phoneNumbers) {
+            add(name, s);
+        }
 
-    public void remove(String name) {
-    }
+        }
 
-    public Boolean hasEntry(String name) {
-        return null;
-    }
+        public void remove (String name){
+        phonebook.remove(name); // this is to remove the name from the phonebook
+        }
 
-    public List<String> lookup(String name) {
-        return null;
-    }
+        public Boolean hasEntry (String name){
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
-    }
+            return phonebook.containsKey(name) ;
+        }
 
-    public List<String> getAllContactNames() {
-        return null;
-    }
+        public Boolean hasEntry (String name, String phoneNumber){
+            if (phonebook.containsKey(name) &&
+                    phonebook.get(name).contains(phoneNumber)) {
+                return true;
+            }
+            return false ;
+        }
 
-    public Map<String, List<String>> getMap() {
-        return null;
+
+        public List<String> lookup (String name){
+            return phonebook.get(name);
+        }
+
+        public String reverseLookup (String phoneNumber) {
+            for (String key : phonebook.keySet()) {
+                //              if(phonebook.get(key).equals(phoneNumber))
+                //                 return key;
+                //         }
+                if (hasEntry(key, phoneNumber)) {
+                    return key;
+                }
+            }
+                return null;
+            }
+
+
+        public List<String> getAllContactNames () {
+        ArrayList b = new ArrayList<>();
+        for (String n : phonebook.keySet()){
+            b.add(n);
+        }
+
+            return b ;
+        }
+
+        public Map<String, List<String>> getMap () {
+            return new HashMap(phonebook);
+        }
+
     }
-}
